14d1abb07ee429739a2ec76bc5225e82a1a579f6
JOO0203 <qtqttq123@gmail.com>
2026-07-09 13:06:38 +0900

    쿨타임, 대쉬(무적판정추가) , 스킬베이스 고도화
    쿨타임 - GameplayEffect -> 게임태그 생성하여 적용
    대쉬 - 기존 skillgameplayability 사용하여 무적판정판 추가
    스킬베이스 고도화
    마우스커서 바라보는쪽으로 스킬사용
    스킬사용시 미리 입력한 위치 이동 취소기능추가

--- LostArk/Source/LostArk/LostArkSkillGameplayAbility.h
+++ LostArk/Source/LostArk/LostArkSkillGameplayAbility.h
@@ -15,9 +15,17 @@ public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
+	virtual UGameplayEffect* GetCooldownGameplayEffect() const override;
+
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill")
 	TSoftObjectPtr<UAnimMontage> SkillMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill|Targeting")
+	bool bRotateToMouseOnActivate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill|Targeting")
+	bool bAbortNavigationMove;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill|Dash")
 	bool bApplyDashForce;
@@ -30,6 +38,9 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill|Dash", meta = (EditCondition = "bApplyDashForce"))
 	bool bIgnoreCollisionDuringDash;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill|Dash", meta = (EditCondition = "bApplyDashForce"))
+	bool bInvincibleDuringDash;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Skill")
 	TSubclassOf<class UGameplayEffect> CooldownEffectClass;
@@ -47,4 +58,10 @@ public:
 	UFUNCTION()
 	virtual void OnMontageInterrupted();
 
+	// 자식 어빌리티들에서 시전 시작 시 정지 및 마우스 회전을 일관되게 처리할 수 있는 공통 헬퍼
+	void HandleActivationBasics(const FGameplayAbilityActorInfo* ActorInfo);
+
 };
+
+
+
